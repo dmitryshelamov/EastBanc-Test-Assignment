f755ef2627020ef777d202a8cb764bfba41bef37
Dmitry Shelamov <dmitryshelamov@gmail.com>
2017-12-02 23:34:27 +0700

    Add core project that will response for domain models
    * Add Item domain model

---
+++ EastBancTestAssignment/EastBancTestAssignment.Core/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EastBancTestAssignment.Core.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+
+        public Item()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
