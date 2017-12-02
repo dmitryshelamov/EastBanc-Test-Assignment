4752542cef74f8136198367e92f29fd6d8a7eec2
Dmitry Shelamov <dmitryshelamov@gmail.com>
2017-12-02 23:44:00 +0700

    Add basic backpackTask domain model

---
+++ EastBancTestAssignment/EastBancTestAssignment.Core/Models/BackpackTask.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EastBancTestAssignment.Core.Models
+{
+    public class BackpackTask
+    {
+        public string Id { get; set; }
+        public Backpack Backpack { get; set; }
+        public List<Item> Items { get; set; }   
+    }
+}
