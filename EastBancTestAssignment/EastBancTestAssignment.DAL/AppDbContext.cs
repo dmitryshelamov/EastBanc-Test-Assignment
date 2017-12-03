03be600c18db633a53ca1d6ea5694527600c551c
Dmitry Shelamov <dmitryshelamov@gmail.com>
2017-12-03 18:25:45 +0700

    Implement IAppDbContext and IUnitOfWork interfaces

---
+++ EastBancTestAssignment/EastBancTestAssignment.DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using EastBancTestAssignment.Core.Models;
+using EastBancTestAssignment.DAL.Interfaces;
+
+namespace EastBancTestAssignment.DAL
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public IDbSet<Item> Items { get; set; }
+        public IDbSet<BackpackTask> BackpackTasks { get; set; }
+
+        public AppDbContext() : base("DefaultConnection") { }
+
+        public AppDbContext(string conectionString) : base(conectionString) { }
+    }
+}
---
+++ EastBancTestAssignment/EastBancTestAssignment.DAL/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using EastBancTestAssignment.DAL.Interfaces;
+using EastBancTestAssignment.DAL.Interfaces.Repositories;
+using EastBancTestAssignment.DAL.Repositories;
+
+namespace EastBancTestAssignment.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public IItemRepository ItemRepository { get; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            ItemRepository = new ItemRepository(context);
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
