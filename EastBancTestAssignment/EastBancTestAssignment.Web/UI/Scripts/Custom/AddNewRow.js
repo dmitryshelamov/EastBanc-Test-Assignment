f7e138827de8b796308ff0ab7ed8ea96a26b51a3
Dmitry Shelamov <dmitryshelamov@gmail.com>
2017-12-06 05:05:32 +0700

    Reset row indexes when we delete a row, so we can pass data back to controller

--- EastBancTestAssignment/EastBancTestAssignment.Web/UI/Scripts/Custom/AddNewRow.js
+++ EastBancTestAssignment/EastBancTestAssignment.Web/UI/Scripts/Custom/AddNewRow.js
@@ -6,7 +6,6 @@
 $(document).ready(function () {
     var $tableItem = $("#table-item");
     var $buttomAddRow = $("#button-add-row");
-    var $buttonDeleteRow = $tableItem.find("tbody > tr button");
 
     $buttomAddRow.on("click",
         function() {
@@ -17,7 +16,8 @@ $(document).ready(function () {
                 .find("tbody > .template")
                 .clone()
                 .removeClass("template")
-                .addClass()
+                .addClass("data-row")
+                .find(".row-number").text(newIndex).end()
                 .find(".name-input").attr("name", "Items[" + newIndex + "].Name").end()
                 .find(".price-input").attr("name", "Items[" + newIndex + "].Price").end()
                 .find(".weight-input").attr("name", "Items[" + newIndex + "].Weight").end()
@@ -27,13 +27,24 @@ $(document).ready(function () {
 });
 
 
-//  user even delegate, because we create rows dynamically
+//  user event delegate, because we create rows dynamically
 $(document).on("click", "tbody > tr button", function () {
+    var $tableItem = $("#table-item");
+
+    
     $(this).closest("tr").remove();
+
+    var itemIndex = 0;
+    $tableItem.find("tbody > .data-row").each(function (index) {
+        var thisRow = $(this);
+
+        thisRow.find(".row-number").text(index);
+        thisRow.find(".item-name").find("input").attr("name", "Items[" + index + "].Name");
+        thisRow.find(".item-price").find("input").attr("name", "Items[" + index + "].Price");
+        thisRow.find(".item-weight").find("input").attr("name", "Items[" + itemIndex + "].Weight");
+
+        itemIndex++;
+    });
+
     return false;
 });
-
-//$buttonDeleteRow.on("click",
-//    function () {
-//        $(this).closest("tr").remove();
-//    });
