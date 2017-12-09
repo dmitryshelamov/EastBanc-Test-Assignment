41bfe498af0a19d373e76f49e1bcdae115f01a22
Dmitry Shelamov <dmitryshelamov@gmail.com>
2017-12-09 12:19:30 +0700

    Code refactoring

--- EastBancTestAssignment/EastBancTestAssignment.KnapsackProblem/App_Start/StartInProgressTasks.cs
+++ EastBancTestAssignment/EastBancTestAssignment.KnapsackProblem/App_Start/StartInProgressTasks.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using EastBancTestAssignment.KnapsackProblem.BLL.Services;
-using EastBancTestAssignment.KnapsackProblem.DAL;
-using EastBancTestAssignment.KnapsackProblem.DAL.Repositories;
+﻿using EastBancTestAssignment.KnapsackProblem.BLL.Services;
 
 namespace EastBancTestAssignment.KnapsackProblem.App_Start
 {
@@ -12,7 +6,7 @@ namespace EastBancTestAssignment.KnapsackProblem.App_Start
     {
         public static void StartTasks()
         {
-            new BackpackTaskService(new UnitOfWork(new AppDbContext())).ContinueInProgressBackpackTasks();
+            new BackpackTaskService().ContinueInProgressBackpackTasks();
         }
     }
 }
